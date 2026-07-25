a42d200710d10824f5e3f0678463956d3d128c5b
Fahrudin <fahroediinj@gmail.com>
2026-07-25 21:42:02 +0700

    perf(bot): add --disable-renderer-backgrounding to eliminate CPU throttling on text commands

--- bot/bot.js
+++ bot/bot.js
@@ -85,6 +85,7 @@ const client = new Client({
             '--disable-background-networking',
             '--disable-background-timer-throttling',
             '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
             '--disable-breakpad',
             '--disable-client-side-phishing-detection',
             '--disable-ipc-flooding-protection',
