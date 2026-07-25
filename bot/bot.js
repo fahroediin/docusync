8249ecb2b58859b2c732d229fac67620c6d35475
Fahrudin <fahroediinj@gmail.com>
2026-07-26 03:03:41 +0700

    fix(bot): add stable webVersionCache, bypassCSP, and desktop User-Agent to eliminate Chromium navigation reload loops

--- bot/bot.js
+++ bot/bot.js
@@ -84,10 +84,15 @@ process.on('unhandledRejection', (reason, promise) => {
 
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: CLIENT_ID }),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014589255-alpha.html'
+    },
     puppeteer: {
         headless: true,
         protocolTimeout: 300000,
         timeout: 120000,
+        bypassCSP: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -95,7 +100,8 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--disable-gpu',
-            '--window-size=1366,768'
+            '--window-size=1366,768',
+            '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
         ]
     }
 });
